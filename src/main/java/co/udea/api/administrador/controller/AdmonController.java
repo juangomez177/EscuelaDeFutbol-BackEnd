b7dccda69840ca4dd692255b17081c3500466c5d
M-Kleber <marfa.kleber@udea.edu.co>
2023-05-08 20:58:13 -0500

    Crear, Eliminar Y Actualizar

--- src/main/java/co/udea/api/administrador/controller/AdmonController.java
+++ src/main/java/co/udea/api/administrador/controller/AdmonController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("equipo")
+@RequestMapping("/equipo")
 public class AdmonController {
     private final Logger log = LoggerFactory.getLogger(AdmonController.class);
 
@@ -31,6 +31,67 @@ public class AdmonController {
         log.info("Rest request buscar Equipo por id: "+ id);
         return ResponseEntity.ok(admonService.getAdmon(id));
     }
+
+    @GetMapping("")
+    @ApiOperation(value = "Buscar todos los Equipos",  response = List.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Equipos encontrado exitosamente"),
+            @ApiResponse(code = 400, message = "La petición es invalida"),
+            @ApiResponse(code = 500, message = "Error interno al procesar la respuesta")})
+    public ResponseEntity<List<Admon>> getEquipos(){
+        log.info("Rest request buscar Equipos");
+        return ResponseEntity.ok(admonService.getAdmon());
+    }
+
+    @GetMapping("buscar/{name}")
+    @ApiOperation(value = "Equipos Encontrdaos",  response = List.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Equipos encontrados exitosamente"),
+            @ApiResponse(code = 400, message = "La petición es invalida"),
+            @ApiResponse(code = 500, message = "Error interno al procesar la respuesta")})
+    public ResponseEntity<List<Admon>> searchEquipos(@PathVariable String name){
+        log.info("Rest request buscar Equipos");
+        return ResponseEntity.ok(admonService.searchEquipos(name));
+    }
+
+
+    /** PUT: update the Equipo on the server */
+    @PutMapping()
+    @ApiOperation(value = "Equipo actualizado",  response = List.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Equipo actualizado exitosamente"),
+            @ApiResponse(code = 400, message = "La petición es invalida"),
+            @ApiResponse(code = 500, message = "Error interno al procesar la respuesta")})
+    public ResponseEntity<Admon> updateEquipo(@RequestBody Admon admon){
+        log.info("Rest request actualizar Equipo");
+        return ResponseEntity.ok(admonService.updateEquipo(admon));
+    }
+
+    /** POST: add a new Equipo to the server */
+    @PostMapping ()
+    @ApiOperation(value = "Agregar Equipo",  response = List.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Equipo agregado exitosamente"),
+            @ApiResponse(code = 400, message = "La petición es invalida"),
+            @ApiResponse(code = 500, message = "Error interno al procesar la respuesta")})
+    public ResponseEntity<Admon> addEquipo(@RequestBody Admon admon){
+        log.info("Rest request agregar Equipo");
+        return ResponseEntity.ok(admonService.addEquipo(admon));
+    }
+
+    /** DELETE: delete the Equipo from the server */
+    @DeleteMapping("{id}")
+    @ApiOperation(value = "Equipo eliminado",  response = List.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Equipo eliminado exitosamente"),
+            @ApiResponse(code = 400, message = "La petición es invalida"),
+            @ApiResponse(code = 500, message = "Error interno al procesar la respuesta")})
+    public void deleteEquipo(@PathVariable Integer id){
+        log.info("Rest request eliminar Equipo");
+        (admonService.deleteEquipo(id);
+    }
+
+
 
 
 }
